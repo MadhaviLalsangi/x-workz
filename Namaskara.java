6db5a1236194df4920908781cf247667eb6647b2
MadhaviLalsangi <72923045+MadhaviLalsangi@users.noreply.github.com>
2020-10-16 17:35:25 +0530

    my first commit

---
+++ Namaskara.java
@@ -0,0 +1,25 @@
+class Wish
+{
+//this is comments
+//To start a program we need a method (main)
+//main method is a entry point
+public static void main(String[] madhavi)
+{
+System.out.println("Namaskara");
+}
+
+
+}
+
+class Camera
+{
+public static void main(String camera[])
+{
+System.out.println("Camera is kodak");
+System.out.println("price is lot");
+
+}
+	
+}
+
+
