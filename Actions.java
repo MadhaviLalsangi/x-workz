2d6d1c0777d04135351d4c90dab87fc5884e26a3
MadhaviLalsangi <72923045+MadhaviLalsangi@users.noreply.github.com>
2020-10-18 18:00:27 +0530

    my third commit

---
+++ Actions.java
@@ -0,0 +1,17 @@
+class Actions
+{
+public static void main(String[] movement)
+{
+System.out.println("enjoy main");
+music();
+}
+public static void music()
+{
+System.out.println("enjoy music");
+drinking();
+}
+public static void drinking()
+{
+System.out.println("enjoy drinking");
+}
+}
