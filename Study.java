309a7894763d9f47ecdd9c9014425d4b6c054ca6
MadhaviLalsangi <72923045+MadhaviLalsangi@users.noreply.github.com>
2020-10-18 18:03:59 +0530

    my fifth commit

---
+++ Study.java
@@ -0,0 +1,14 @@
+class Study
+{
+public static void main(String[] subjects)
+{
+System.out.println("exam main");
+write();
+write();
+write();
+}
+public static void write()
+{
+System.out.println("exam write");
+}
+}
