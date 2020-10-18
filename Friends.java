d2a32bfa336581ac19a8715cfe049b2f74438832
MadhaviLalsangi <72923045+MadhaviLalsangi@users.noreply.github.com>
2020-10-18 18:01:57 +0530

    my fourth commit

---
+++ Friends.java
@@ -0,0 +1,32 @@
+class Friends
+{
+public static void main(String[] loyal)
+{
+System.out.println("trust main");
+enjoyment();
+}
+public static void enjoyment()
+{
+System.out.println("trust enjoyment");
+hangout();
+}
+public static void hangout()
+{
+System.out.println("trust hangout");
+masti();
+}
+public static void masti()
+{
+System.out.println("trust masti");
+trips();
+}
+public static void trips()
+{
+System.out.println("trust trips");
+happiness();
+}
+public static void happiness()
+{
+System.out.println("trust happiness");
+}
+}
