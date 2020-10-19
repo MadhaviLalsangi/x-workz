581735b62b84afd80557bff802e0676b9a41e486
MadhaviLalsangi <72923045+MadhaviLalsangi@users.noreply.github.com>
2020-10-19 22:36:54 +0530

    information pgm

---
+++ Information.java
@@ -0,0 +1,31 @@
+public class Information
+{
+    public static void main(String[] args)
+    {
+      System.out.println("info");
+byte fanBlades;
+fanBlades=3;
+short daysInYear;
+daysInYear=365;
+int averageHairOnHumanHead;
+averageHairOnHumanHead=30000;
+long numberOfTrees;
+numberOfTrees=30410000;
+float unEmploymentRate;
+unEmploymentRate=8.87f;
+double areaOfEarth;
+areaOfEarth=345.567d;
+boolean snakeIsBird;
+snakeIsBird=false;
+char position;
+position='A';
+System.out.println(fanBlades);
+System.out.println(daysInYear);
+System.out.println(averageHairOnHumanHead);
+System.out.println(numberOfTrees);
+System.out.println(unEmploymentRate);
+System.out.println(areaOfEarth);
+System.out.println(snakeIsBird);
+System.out.println(position);
+}
+}
