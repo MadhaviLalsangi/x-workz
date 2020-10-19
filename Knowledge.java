5e7855b3133c60fa3c1cf36490486404a67a91c0
MadhaviLalsangi <72923045+MadhaviLalsangi@users.noreply.github.com>
2020-10-19 22:37:41 +0530

    knowledge pgm

---
+++ Knowledge.java
@@ -0,0 +1,31 @@
+public class Knowledge
+{
+    public static void main(String[] args)
+    {
+      System.out.println("world");
+byte octopusHeart;
+octopusHeart=3;
+short totalLanguages;
+totalLanguages=6500;
+int primarySchools;
+primarySchools=809108;
+long lightSpeed;
+lightSpeed=123456717;
+float youthLiteracyRate;
+youthLiteracyRate=91f;
+double surfaceAreaOfSun;
+surfaceAreaOfSun=510.1d;
+boolean milkIsBlack;
+milkIsBlack=false;
+char secondClass;
+secondClass='B';
+System.out.println(octopusHeart);
+System.out.println(totalLanguages);
+System.out.println(primarySchools);
+System.out.println(lightSpeed);
+System.out.println(youthLiteracyRate);
+System.out.println(surfaceAreaOfSun);
+System.out.println(milkIsBlack);
+System.out.println(secondClass);
+}
+}
