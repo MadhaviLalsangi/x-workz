7d0a885c17bac405c76fad30805d15f83e990198
MadhaviLalsangi <72923045+MadhaviLalsangi@users.noreply.github.com>
2020-10-19 22:36:00 +0530

    world pgm

---
+++ World.java
@@ -0,0 +1,31 @@
+public class World
+{
+    public static void main(String[] args)
+    {
+      System.out.println("Measurements");
+byte fingers;
+fingers=10;
+short daysInYear;
+daysInYear=365;
+int numberOfEngineersInIndia;
+numberOfEngineersInIndia=50000;
+long populationOfIndia;
+populationOfIndia=423498745;
+float percentageOfStudent;
+percentageOfStudent=8.57f;
+double chargeOfElectron;
+chargeOfElectron=32.12345d;
+boolean earthIsFlat;
+earthIsFlat=false;
+char grade;
+grade='A';
+System.out.println(fingers);
+System.out.println(daysInYear);
+System.out.println(numberOfEngineersInIndia);
+System.out.println(populationOfIndia);
+System.out.println(percentageOfStudent);
+System.out.println(chargeOfElectron);
+System.out.println(earthIsFlat);
+System.out.println(grade);
+}
+}
