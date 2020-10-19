df797033fd84f3beb9ecf33a8efea21a1a94b552
MadhaviLalsangi <72923045+MadhaviLalsangi@users.noreply.github.com>
2020-10-19 22:39:46 +0530

    technology pgm

---
+++ Technology.java
@@ -0,0 +1,31 @@
+public class Technology
+{
+    public static void main(String[] args)
+    {
+      System.out.println("objects");
+byte eyes;
+eyes=2;
+short heightOfMountEverest ;
+heightOfMountEverest=29017;
+int heartPumpsBlood;
+heartPumpsBlood=100000;
+long numberOfTreesInIndia;
+numberOfTreesInIndia=35000000;
+float literacyRateIn2020;
+literacyRateIn2020=77.7f;
+double moonsSurfaceArea;
+moonsSurfaceArea=14.6d;
+boolean asiaIsNotAContinent;
+asiaIsNotAContinent=false;
+char firstClass;
+firstClass='A';
+System.out.println(eyes);
+System.out.println(heightOfMountEverest);
+System.out.println(heartPumpsBlood);
+System.out.println(numberOfTreesInIndia);
+System.out.println(literacyRateIn2020);
+System.out.println(moonsSurfaceArea);
+System.out.println(asiaIsNotAContinent);
+System.out.println(firstClass);
+}
+}
