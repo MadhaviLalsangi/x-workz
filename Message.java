4d536e1b33dac9ff4f494f00b06b64c24881c695
MadhaviLalsangi <72923045+MadhaviLalsangi@users.noreply.github.com>
2020-10-20 21:23:11 +0530

    methods-message

---
+++ Message.java
@@ -0,0 +1,17 @@
+public class Message
+{
+public static void main(String[] c)
+{
+System.out.println("Madhavi");
+printEmail("madhavi.xworkz@gmail.com");
+printProgrammingLanguage("java");
+}
+public static void printEmail(String email)
+{
+System.out.println("Email:" +email);
+}
+public static void printProgrammingLanguage(String programmingLanguage)
+{
+System.out.println("programmingLanguage:" +programmingLanguage);
+}
+}
