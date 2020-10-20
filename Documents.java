a093735548a3a8a9b7288b9efc19b3f096c2bf59
MadhaviLalsangi <72923045+MadhaviLalsangi@users.noreply.github.com>
2020-10-20 21:21:11 +0530

    methods-documents

---
+++ Documents.java
@@ -0,0 +1,14 @@
+public class Documents
+{
+public static void main(String[] maddy)
+{
+final String adharNumber="7896 7865 8907";
+
+final String mobileNumber="8765439235";
+final String emailId="madhavi.xworkz@gmail.com";
+
+System.out.println("adharNumber :"+adharNumber);
+System.out.println("mobileNumber :"+mobileNumber);
+System.out.println("emailId :"+emailId);
+}
+}
