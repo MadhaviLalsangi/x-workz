1f679f2a46720cc10749f179f1d7b54c9fd0e2a9
MadhaviLalsangi <72923045+MadhaviLalsangi@users.noreply.github.com>
2020-10-20 21:22:13 +0530

    methods-greet

---
+++ Greet.java
@@ -0,0 +1,12 @@
+public class Greet
+{
+public static void main(String[] c)
+{
+System.out.println("gratitude");
+printGreetings("welcome");
+}
+public static void printGreetings(String greetings)
+{
+System.out.println("Greetings:" +greetings);
+}
+}
